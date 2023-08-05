a88ac2506ad2e162187277356764f837741e3578
Satyxa <satyxa1919@gmail.com>
2023-08-06 00:17:46 +0300

    Commit message.

--- Frontend/src/Components/Content/MainPage/Brand.tsx
+++ Frontend/src/Components/Content/MainPage/Brand.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 import './Brand.css'
-import brand from '../../../../src/assets/sthule.jpg'
+import brand from '../../../../src/assets/brand2.jpg'
 import {useSelector} from "react-redux";
 import Menu from "../../Menu/Menu";
+import MenuPlace from "../../Menu/MenuPlace";
 
 const Brand: React.FC = () => {
 
     const menu = useSelector(state => state.menu)
 
     return (
-        <div className='brand__field'>
-            <div className="brand__field__img">
-                <img src={brand} width='800' height='765' alt="huita"/>
-            </div>
-            <div className="brand__field__title">
-                <div className="title__box">
+        <div>
+            <MenuPlace/>
+            <div className='brand__field'>
 
-                    <h1>THE BRAND</h1>
-                    <h2>OF LUXERIOUS FASHION</h2>
-
+                <div className="brand__field__img">
+                    <img src={brand} width='800' height='765'/>
                 </div>
+                <div className="brand__field__title">
+                    <div className="title__box">
+
+                        <h1>THE BRAND</h1>
+                        <h2>OF LUXERIOUS FASHION</h2>
+
+                    </div>
+                </div>
+                <Menu/>
             </div>
-            <Menu/>
         </div>
+
     );
 };
 
